8d4051ad81ce18506ea84a5bd9c73d103e017cac
josh <abcbppiaglumf7@gmail.com>
2026-03-17 22:53:08 -0500

    move kotlin

--- kotlin/src/main/kotlin/com/joshy/hywrapper/data/skyblock/GardenUpgrade.kt
+++ kotlin/src/main/kotlin/com/joshy/hywrapper/data/skyblock/GardenUpgrade.kt
@@ -7,8 +7,10 @@ import kotlinx.serialization.Serializable
 enum class GardenUpgrade(val cleanName: String) {
     @SerialName("GROWTH_SPEED")
     GROWTH_SPEED("Crop Growth Speed"),
+
     @SerialName("YIELD")
     CROP_UPGRADE("Crop Upgrade"),
+
     @SerialName("PLOT_LIMIT")
     PLOT_LIMIT("Plot Limit")
 }
--- kotlin/src/main/kotlin/com/joshy/hywrapper/model/playerdata/player/Stats.kt
+++ kotlin/src/main/kotlin/com/joshy/hywrapper/model/playerdata/player/Stats.kt
@@ -1,5 +1,3 @@
 package com.joshy.hywrapper.model.playerdata.player
 
-import kotlinx.serialization.Serializable
-
 // TODO add stats here cuz what the hell
