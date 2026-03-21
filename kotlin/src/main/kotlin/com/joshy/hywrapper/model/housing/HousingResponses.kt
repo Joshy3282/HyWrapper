cddd7d64182893daa912c5f30188e5de4572d4ec
josh <abcbppiaglumf7@gmail.com>
2026-03-20 22:10:24 -0500

    housing documentation

--- kotlin/src/main/kotlin/com/joshy/hywrapper/model/housing/HousingResponses.kt
+++ kotlin/src/main/kotlin/com/joshy/hywrapper/model/housing/HousingResponses.kt
@@ -5,6 +5,13 @@ import com.joshy.hywrapper.model.RateLimit
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
+/**
+ * List of all active Houses.
+ *
+ * @property success Whether the request was successful.
+ * @property cause The cause of the error, if the request failed.
+ * @property houses A list of [House].
+ */
 @Serializable
 data class HousingActiveResponse(
     override val success: Boolean? = null,
@@ -15,6 +22,13 @@ data class HousingActiveResponse(
     override var rateLimit: RateLimit? = null
 }
 
+/**
+ * Information about a House.
+ *
+ * @property success Whether the request was successful.
+ * @property cause The cause of the error, if the request failed.
+ * @property house The [House] details.
+ */
 @Serializable
 data class HousingHouseResponse(
     override val success: Boolean? = null,
@@ -25,6 +39,16 @@ data class HousingHouseResponse(
     override var rateLimit: RateLimit? = null
 }
 
+/**
+ * House information
+ *
+ * @property uuid The UUID of the house.
+ * @property owner The UUID of the player who owns the house.
+ * @property name The display name of the house.
+ * @property createdAt The timestamp when the house was created.
+ * @property players The current number of players in the house.
+ * @property cookies The [Cookies] statistics for the house.
+ */
 @Serializable
 data class House(
     val uuid: String? = null,
@@ -35,11 +59,23 @@ data class House(
     val cookies: Cookies? = null,
 )
 
+/**
+ * Cookie information.
+ *
+ * @property current The current number of cookies the house has received.
+ */
 @Serializable
 data class Cookies(
     val current: Int? = null,
 )
 
+/**
+ * List of a player's houses.
+ *
+ * @property success Whether the request was successful.
+ * @property cause The cause of the error, if the request failed.
+ * @property houses A list of [House] belonging to the player.
+ */
 @Serializable
 data class HousingHousesResponse(
     override val success: Boolean? = null,
